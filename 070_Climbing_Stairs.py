9124f0ba83bf05f568807933c3d6fa72d146cb49
dawnstar <dev@dawnstar.me>
2016-09-02 14:29:26 +0800

    Solve 70. Climbing Stairs

---
+++ 070_Climbing_Stairs.py
@@ -0,0 +1,18 @@
+# You are climbing a stair case. It takes n steps to reach to the top.
+#
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+#
+# Tags: Dynamic Programming
+# Difficulty: Easy
+
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a, b = 0, 1
+        for _ in xrange(n):
+            a, b = b, a + b
+        return b
