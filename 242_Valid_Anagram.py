542823999e77e3b84682f41725d63465662614d7
dawnstar <dev@dawnstar.me>
2016-09-07 15:16:53 +0800

    Solve 242. Valid Anagram

---
+++ 242_Valid_Anagram.py
@@ -0,0 +1,30 @@
+# Given two strings s and t, write a function to determine if t is an anagram of s.
+#
+# For example,
+# s = "anagram", t = "nagaram", return true.
+# s = "rat", t = "car", return false.
+#
+# Note:
+# You may assume the string contains only lowercase alphabets.
+#
+# Follow up:
+# What if the inputs contain unicode characters? How would you adapt your solution to such case?
+#
+# Tags: Hash Table, Sort
+# Difficulty: Easy
+
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        return self.freq(s) == self.freq(t)
+
+    def freq(self, s):
+        freq = {}
+        for c in s:
+            freq[c] = freq.get(c, 0) + 1
+        return freq
